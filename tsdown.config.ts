35835ffc1780b627f4c8d211fd33e5391cfe89b1
Igor Pnev <tkospb@gmail.com>
2025-10-03 08:40:55 +0300

    Bundle all dependencies and bump to v1.0.5

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -7,4 +7,14 @@ export default defineConfig({
   outDir: 'dist',
   clean: true,
   shims: false,
+  noExternal: true,
+  external: [/^node:/, 'react-devtools-core'],
+  treeshake: {
+    moduleSideEffects: false,
+  },
+  rolldownOptions: {
+    output: {
+      inlineDynamicImports: true,
+    },
+  },
 })
